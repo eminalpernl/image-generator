618fa55ba85b21f16b4a700b2917886d876d88b3
eminalpernl <eminalpernl@gmail.com>
2023-05-20 14:56:01 +0200

    openai api is set

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import generateImage from "./generate-image.js";
 import express from "express";
 
 const app = express();
@@ -8,6 +9,26 @@ const port = process.env.PORT || 3005;
 
 app.get("/", (req, res) => {
   res.send("Hello world from Server");
+});
+
+app.post("/generate", async (req, res) => {
+  const prompt = req.body.prompt;
+  try {
+    const image = await generateImage(prompt);
+    res.json({ response: image });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: "the image could not be generated",
+    });
+  }
 });
 
 app.listen(port, () => {
